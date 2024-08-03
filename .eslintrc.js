e4d894d13d8e5e4f1d2ae545c9d3108b9f02b3cb
Camila <camila.vargasn@gmail.com>
2024-08-03 18:57:19 -0300

    alterções

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: [
+    'vue'
+  ],
+  extends: [
+    'plugin:vue/recommended'
+  ]
+}
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,45 @@
 const mix = require('laravel-mix');
+const path = require('path');
+const webpack = require('webpack');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .vue()
+    .sass('resources/sass/app.scss', 'public/css')
+    .options({
+        processCssUrls: false,
+        postCss: [
+            require('autoprefixer'),
+        ],
+    })
+    .version();
+
+
+mix.webpackConfig({
+    resolve: {
+        alias: {
+            '@': path.resolve('resources/js'),
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    compilerOptions: {
+                        compatConfig: {
+                            MODE: 3
+                        }
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            __VUE_OPTIONS_API__: true,
+            __VUE_PROD_DEVTOOLS__: false,
+        }),
+    ],
+});
